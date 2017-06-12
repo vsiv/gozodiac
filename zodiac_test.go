4e285dfe252441223e6f5f9402b8c47a838b18e9
Vishal Shah <v@vstarlabs.com>
2017-06-12 15:22:58 +0530

    tests

--- zodiac_test.go
+++ zodiac_test.go
@@ -1,13 +1,25 @@
 package gozodiac
 
 import (
-	"log"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetZodiacSign(t *testing.T) {
+const shortForm = "2006-Jan-02"
 
-	sign := GetZodiacSign(time.Now())
-	log.Printf("sign: %v", sign)
+func TestGetZodiacSign(t *testing.T) {
+	// sign := GetZodiacSign(time.Now())
+	// log.Printf("sign: %v", sign)
+
+	testTime, err := time.Parse(shortForm, "2013-Feb-03")
+	assert.NoError(t, err)
+	sign := GetZodiacSign(testTime)
+	assert.Equal(t, []ZodiacSign{Aquarius}, sign)
+
+	testTime, err = time.Parse(shortForm, "2001-Nov-15")
+	assert.NoError(t, err)
+	sign = GetZodiacSign(testTime)
+	assert.Equal(t, []ZodiacSign{Scorpio}, sign)
 }
